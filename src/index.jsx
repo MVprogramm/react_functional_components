4c861102d83e00b03812a431a8cd5303e2cdea43
Michael Valiahmetov <michael.valiahmetov@gmail.com>
2022-05-29 11:08:18 +0300

    add functional component

---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+import Search from "./Search.jsx";
+
+const rootElem = document.querySelector("#root");
+
+ReactDOM.render(<Search name="Michael" />, rootElem);
+
